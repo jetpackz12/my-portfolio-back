a90f6596989a82abcafd57e68112281d9f3123be
Joseph Jett Abela <jetpackzabela12@gmail.com>
2026-03-18 17:19:25 +0800

    refactor/OfferSeederAddingNewDatas

--- database/seeders/OfferSeeder.php
+++ database/seeders/OfferSeeder.php
@@ -17,5 +17,35 @@ class OfferSeeder extends Seeder
             'description' => "Creating visually appealing, responsive, and user-friendly interfaces using HTML, CSS, Bootstrap, Tailwind CSS, JavaScript, jQuery, Ajax, and modern frameworks like Vue.js.",
             'icon' => "display",
         ]);
+
+        Offer::create([
+            'title' => "Back-End Development",
+            'description' => "Building robust and scalable server-side applications with PHP, Laravel and databases like MySQL.",
+            'icon' => "laptop-code",
+        ]);
+        
+        Offer::create([
+            'title' => "Full-Stack Development",
+            'description' => "Handling both front-end and back-end development to deliver complete web solutions.",
+            'icon' => "code",
+        ]);
+        
+        Offer::create([
+            'title' => "API Integration",
+            'description' => "Connecting and integrating third-party APIs to enhance website functionality.",
+            'icon' => "network-wired",
+        ]);
+        
+        Offer::create([
+            'title' => "Website Deployment",
+            'description' => "Deploying websites using Hostinger hosting platforms, ensuring smooth and reliable launches.",
+            'icon' => "cloud-arrow-up",
+        ]);
+        
+        Offer::create([
+            'title' => "Maintenance & Support",
+            'description' => "Providing ongoing maintenance, updates, and technical support for existing websites.",
+            'icon' => "screwdriver-wrench",
+        ]);
     }
 }
